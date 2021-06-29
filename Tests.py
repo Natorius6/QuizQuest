52b2fd50f017218f171ae63838171b7012fea679
Charley Cates <CCates@stu.kerikerihigh.ac.nz>
2021-06-30 09:55:38 +1200

    testing hashing

--- Tests.py
+++ Tests.py
@@ -1,10 +1,11 @@
+import hashlib
 f= open("QuizQuest/Username.py", "a")
 
 
 Username = input("please enter a username. please note that no numbers are allowed. \n")
 Password = input("please enter a password. please note that no numbers are allowed. \n")
-hash(Password)
-f.write(f"{Username} = '{Password}'\n")
+hashed_password = hashlib.sha224(f"{Password}").hexdigest()
+f.write(f"{Username} = '{hashed_password}'\n")
 
 #f= open("//dataserver2/CCates$/dev/QuizQuest/file.txt", "w+")
 #for i in range(10):
