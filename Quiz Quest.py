159ed0a03effcab2ec3804122b6cf9eb1cc0e646
Charley Cates <CCates@stu.kerikerihigh.ac.nz>
2021-07-08 10:55:01 +1200

    did some quality of life improvements

--- Quiz Quest.py
+++ Quiz Quest.py
@@ -20,41 +20,43 @@ while True:
 
     questions = response.json()['results']
 
+    logged_in = False
 
-
-
+    quiz_finished = False
+    num_questions = 0
     num_correct = 0
 
-        
-    new_user = get_integer_input("would you like to create a new user or login to existing user. \n1 new user. \n2 login to existing user. \n", "please enter 1 or 2 to select your answer", 0, 3)
+    while logged_in == False:
+        new_user = get_integer_input("would you like to create a new user or login to existing user. \n1 new user. \n2 login to existing user. \n", "please enter 1 or 2 to select your answer", 0, 3)
+        if new_user == 1:
+            f= open("QuizQuest/Username.txt", "a")
+            username = input("please enter a username. please note that no numbers are allowed. \n")
+            password = input("please enter a password. please note that no numbers are allowed. \n")
+            os.system('cls')
+            password_hashing = hashlib.sha224(Password.encode())
+            hashed = password_hashing.hexdigest()
+            f.write(f"{username} {hashed} \n")
+            f.close()
 
-    if new_user == 1:
-        f= open("QuizQuest/Username.txt", "a")
-        Username = input("please enter a username. please note that no numbers are allowed. \n")
-        Password = input("please enter a password. please note that no numbers are allowed. \n")
-        os.system('cls')
-        Password_hashing = hashlib.sha224(Password.encode())
-        hashed = Password_hashing.hexdigest()
-        f.write(f"{Username} {hashed} \n")
-        f.close()
-
-    if new_user == 2:
-        f= open("QuizQuest/Username.txt", "r")
-        Username = input("please enter your username. \n")
-        Password = input("please enter your password. \n")
-        os.system('cls')
-        Password_hashing = hashlib.sha224(Password.encode())
-        hashed = Password_hashing.hexdigest()
-        lines = f.readlines()
-        clean_lines = []
-        for line in lines:
-            arr = line.split(' ')
-            clean_lines.append(arr[0:2])
-        for user, password in clean_lines:
-            if user == Username and password == hashed:
-                print("login successful")
-            if user != Username or password != hashed:
-                print("username or password incorrect")
+        if new_user == 2:
+            f= open("QuizQuest/Username.txt", "r")
+            username = input("please enter your username. \n")
+            password = input("please enter your password. \n")
+            os.system('cls')
+            password_hashing = hashlib.sha224(password.encode())
+            hashed = password_hashing.hexdigest()
+            lines = f.readlines()
+            clean_lines = []
+            for line in lines:
+                arr = line.split(' ')
+                clean_lines.append(arr[0:2])
+            for user, password in clean_lines:
+                if user == username and password == hashed:
+                    print("login successful")
+                    logged_in = True
+                if user != username or password != hashed:
+                    print("username or password incorrect")
+            f.close()
 
     print('''\033[1;32;40m
     *****************************WELCOME TO THE \033[1;31;40m QUIZ \033[1;32;40m QUEST*********************************
@@ -70,23 +72,30 @@ while True:
             all_answers.append(ans)
         random.shuffle(all_answers)
         for num, ans in enumerate(all_answers):
-            print(f"{num+1}: {ans}")
-        correct_num = all_answers.index(question['correct_answer']) + 1
-        user_answer = get_integer_input("\n", "that is not one of the options", 0, 5)
-        if user_answer == correct_num:
+            print(unescape(f"{num+1}: {ans}"))
+        correct_ans = all_answers.index(question['correct_answer']) + 1
+        user_answer = get_integer_input("\n", "answer the question with 1-4.", 0, 5)
+        if user_answer == correct_ans:
             print("you got the right answer")
             num_correct += 1
-        elif user_answer != num:
+            num_questions += 1
+        elif user_answer != correct_ans:
             print("you got it wrong try the next question")
-        print(correct_num)
-    if num_correct == 10:
-        replay = input("congrats you got a perfect score. You are clearly a genius. \n Press enter to replay. \n")
-    if 6 <= num_correct >= 9:
-        replay = input("You nearly got a perfect score. So close \n Press enter to replay. \n")
-    if 2 <= num_correct >= 5:
-        replay = input("Nearly half points. Maybe try again. \n Press enter to replay. \n")
-    if num_correct == 1:
-        replay = input("You got one point \n Press enter to replay. \n")
+            num_questions += 1
+        print(correct_ans)
+        if num_questions == 10:
+            quiz_finished = True
+    if quiz_finished == True:
+        if num_correct == 10:
+            replay = input("congrats you got a perfect score. You are clearly a genius. \n Press enter to replay. \n")
+        if 6 <= num_correct >= 9:
+            replay = input(f"You got {num_correct}. So close \n Press enter to replay. \n")
+        if 2 <= num_correct >= 5:
+            replay = input(f"You got {num_correct}. Maybe try again. \n Press enter to replay. \n")
+        if num_correct == 1:
+            replay = input(f"You got {num_correct} \n Press enter to replay. \n")
+        f= open("QuizQuest/Scores.txt", "a")
+        f.write(f"{username}score {num_correct} \n")
 
 # Ideas
 # add multiple rounds
